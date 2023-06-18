3799c27c3d8c92e1c5d18ea51e0a8fae1fe0ab63
Crowdbotics <engineering@crowdbotics.com>
2023-06-18 23:15:24 +0000

    Initial commit.

---
+++ backend/hidden_field_dev_92655/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hidden_field_dev_92655 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hidden_field_dev_92655.settings')
+
+application = get_wsgi_application()
